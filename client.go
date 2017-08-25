c6abfe6bb0fd0cd183ecf601c85f07f60f354457
Septs <github@septs.pw>
2017-08-25 09:14:46 +0800

    modify

--- client.go
+++ client.go
@@ -1,11 +1,9 @@
 package proxy
 
 import (
-	"encoding/binary"
 	"fmt"
 	"net"
 	"net/url"
-	"strconv"
 	"strings"
 )
 
@@ -15,7 +13,7 @@ type Client interface {
 
 func NewClient(proxy *url.URL, conf *SOCKSConf) (client Client, err error) {
 	switch strings.ToUpper(proxy.Scheme) {
-	case "SOCKS", "SOCKS4", "SOCKS4A":
+	case "SOCKS4", "SOCKS4A":
 		client = &socks4Client{proxy, conf}
 	case "SOCKS5", "SOCKS5+TLS":
 		client = &socks5Client{proxy, conf, conf.TLSConfig}
@@ -30,8 +28,5 @@ func splitHostPort(addr string) (host, port []byte, err error) {
 	if err != nil {
 		return
 	}
-	_port, err := strconv.ParseUint(hostPort, 10, 16)
-	_portBuffer := make([]byte, 2)
-	binary.BigEndian.PutUint16(_portBuffer, uint16(_port))
-	return []byte(hostName), _portBuffer, err
+	return []byte(hostName), toPort(hostPort), err
 }
